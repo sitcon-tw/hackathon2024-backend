86cc35d1e15b78ef044481f9fc8e9ffcc11141f2
SiriusKoan <me@siriuskoan.one>
2024-06-23 06:33:02 -0400

    feat: add stamp route

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
-from flask import Flask, Blueprint
-
+import os
+from datetime import timedelta
+from flask import Flask
 
 def create_app(env):
     app = Flask(__name__)
+    app.config['SECRET_KEY'] = os.urandom(24)
+    app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=31)
 
     from .views import bp
     app.register_blueprint(bp, url_prefix="/api")
--- app/views.py
+++ app/views.py
@@ -1,10 +1,13 @@
-from flask import Blueprint
+from flask import Blueprint, send_from_directory, session
+
 
 bp = Blueprint("main", __name__)
 
 @bp.route("/login", methods=["POST"])
 def login_page():
-    ...
+    # placeholder
+    session["username"] = "test"
+    return ""
 
 @bp.route("/guess/<string:problem>", methods=["POST"])
 def guess_page(problem):
@@ -16,4 +19,7 @@ def collect_page():
 
 @bp.route("/stamp/<int:number>", methods=["GET"])
 def stamp_page(number):
-    ...
+    filename = "black.png"
+    if session.get("username", None):
+        filename = f"image-{number}.png"
+    return send_from_directory("../images", filename)
