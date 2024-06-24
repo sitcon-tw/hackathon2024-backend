bb2559758731d19b45b4237d4dd3daac3278e8ea
Yuan <yuan@ncse.tw>
2024-06-24 14:26:34 +0800

    update /login and //guess

--- app/views.py
+++ app/views.py
@@ -5,12 +5,37 @@ bp = Blueprint("main", __name__)
 
 @bp.route("/login", methods=["POST"])
 def login_page():
+    data = request.get_json()
+    user_token = data.get("user_token", None)
+    # query database
+    valid_account = True
+    
+    if not valid_account:
+        return "", 400
+    if True:
+        session["user_token"] = user_token
+        return "", 200
     # placeholder
     session["username"] = "test"
-    return ""
 
 @bp.route("/guess/<string:problem>", methods=["POST"])
 def guess_page(problem):
+    data = request.get_json()
+    answer = data.get("answer", None)
+    if len(answer) != 4:
+        message = {"message": "系統錯誤"}
+        return jsonify(message), 400
+    
+    # query database
+    correct_answer = True
+
+    if not correct_answer:
+        message = {"message": "答案錯誤"}
+        return jsonify(message), 403
+    if True:
+        message = {"message": "答案正確"}
+        return jsonify(message), 200
+
     ...
 
 @bp.route("/collect", methods=["POST"])
