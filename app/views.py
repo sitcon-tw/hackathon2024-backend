544fe703ad4d42e6646e88cc903e90c4c0bdb0ba
SiriusKoan <me@siriuskoan.one>
2024-06-29 08:56:49 -0400

    fix: add token to problem map

--- app/views.py
+++ app/views.py
@@ -8,6 +8,10 @@ client = MongoClient(f"mongodb://{username}:{password}@mongodb:27017/")
 database = client["sitcon-hackathon"]
 collection = database["users"]
 
+TOKEN_PROBLEM_MAP = {
+    "TOKEN0": 0,
+}
+
 bp = Blueprint("main", __name__)
 
 @bp.route("/login", methods=["POST"])
@@ -22,8 +26,6 @@ def login_page():
     if True:
         session["user_token"] = user_token
         return "", 200
-    # placeholder
-    session["username"] = "test"
 
 @bp.route("/guess/<int:problem>", methods=["POST"])
 def guess_page(problem):
@@ -47,26 +49,29 @@ def guess_page(problem):
 
 @bp.route("/collect", methods=["POST"])
 def collect_page():
-    username = session.get("username", "test")
-    if not username:
+    user_token = session.get("user_token", None)
+    if not user_token:
         return "", 401
     data = request.get_json()
     token = data.get("token", None)
-    if not token:
+    problem = TOKEN_PROBLEM_MAP.get(token, None)
+    if not problem:
         return "", 400
-    res = collection.find_one({"username": username})
+    res = collection.find_one({"user_token": user_token})
     if not res:
-        collection.insert_one({"username": username, "tokens": [token]})
+        collection.insert_one({"user_token": user_token, "collected": [problem]})
         return ""
-    elif token in res.get("tokens", []):
+    elif problem in res.get("collected", []):
         return "", 201
     else:
-        collection.update_one({"username": username}, {"$addToSet": {"tokens": token}})
+        collection.update_one({"user_token": user_token}, {"$addToSet": {"collected": problem}})
         return ""
 
 @bp.route("/stamp/<int:number>", methods=["GET"])
 def stamp_page(number):
     filename = "black.png"
-    if session.get("username", None):
-        filename = f"image-{number}.png"
+    if session.get("user_token", None):
+        res = collection.find_one({"user_token": session["user_token"]})
+        if res and number in res.get("collected", []):
+            filename = f"image-{number}.png"
     return send_from_directory("../images", filename)
