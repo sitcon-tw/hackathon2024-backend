3a3be20b42b033b8fb76d3b9d92d81933957f594
Ji Kuai <rk427454171@gmail.com>
2024-07-04 00:21:22 +0800

    chore: modify run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 export FLASK_APP=manage.py
 export FLASK_ENV=development
 export SECRET=very_secret_string
-docker run -itd --name hackathon2024-backend -v .:/app .
+docker build -t hackathon2024-backend .
+docker run --env FLASK_APP --env FLASK_ENV --env SECRET -itd --name hackathon2024-backend -v .:/app -p 127.0.0.1:5011:5000 .
