c09517adb29184f1a021cb0c8262c641614b2470
Ji Kuai <rk427454171@gmail.com>
2024-07-04 02:20:04 +0800

    chore: remove containers and images before building new ones

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
+docker stop hackathon2024-backend
+docker rm hackathon2024-backend
+docker image rm hackathon2024-backend
 export FLASK_APP=manage.py
 export FLASK_ENV=development
 export SECRET=very_secret_string
