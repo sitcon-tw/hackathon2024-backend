df20af717dcc0dda67df44bd503fa5b784721cf2
Ji Kuai <rk427454171@gmail.com>
2024-07-04 02:49:19 +0800

    feat: remove other featues since the remote now supports docker compose

--- app/views.py
+++ app/views.py
@@ -7,10 +7,7 @@ secret = os.getenv("SECRET")
 assert secret
 username = os.getenv("MONGO_USERNAME", "admin")
 password = os.getenv("MONGO_PASSWORD", "admin")
-if os.getenv("FLASK_ENV", "production") == 'production':
-    client = MongoClient(f"mongodb://172.17.0.1:27017/")
-else:
-    client = MongoClient(f"mongodb://{username}:{password}@mongodb:27017/")
+client = MongoClient(f"mongodb://{username}:{password}@mongodb:27017/")
 collection = client["sitcon-hackathon"]["users"]
 
 TOKEN_PROBLEM_MAP = {
--- run.sh
+++
@@ -1,8 +0,0 @@
-docker stop hackathon2024-backend
-docker rm hackathon2024-backend
-docker image rm hackathon2024-backend
-export FLASK_APP=manage.py
-export FLASK_ENV=development
-export SECRET=very_secret_string
-docker build -t hackathon2024-backend .
-docker run --env FLASK_APP --env FLASK_ENV --env SECRET -itd --name hackathon2024-backend -v .:/app --network bridge_for_hackathon2024 -p 127.0.0.1:5011:5000 hackathon2024-backend
