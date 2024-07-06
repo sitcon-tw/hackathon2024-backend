26af71a64c78bb4c172cd77bb8fa40a3eb0979ef
Denny Huang <denny0223@gmail.com>
2024-07-06 17:42:52 +0800

    fix: session expire time

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 import os
-from datetime import timedelta
+from datetime import datetime, timedelta
 from flask import Flask
 from flask_cors import CORS
 
 def create_app(env):
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.urandom(24)
-    app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=31)
+    app.config['PERMANENT_SESSION_LIFETIME'] = datetime.now() + timedelta(days=31)
 
     from .views import bp
     app.register_blueprint(bp, url_prefix="/api")
